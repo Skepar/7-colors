853ef96cb247f407d1e5e66e3a048260713a9c34
Pierre <pierrebourse59@gmail.com>
2019-10-05 15:53:15 +0200

    question 4

--- 7colors.c
+++ 7colors.c
@@ -18,6 +18,8 @@ void init_board(char board[SIZE][SIZE], int size) {
 			      board[i][j]=rand()%7+65;
         }
     }
+    board[SIZE-1][0]='v';
+    board[0][SIZE-1]='^';
 }
 
 /** Returns 1 if the cell (i,j) is adjacent to a cell possessed by player*/
@@ -45,6 +47,14 @@ int player_adjacent(char player_symbol,int i,int j, char board[SIZE][SIZE]) {
     return 0;
   }
 
+char symbol(int player) {
+  if (player == 0) {
+    return 'v';
+  } else {
+    return '^';
+  }
+}
+
 /**Updates the board when a player has played and chosen a color
 *Note : The player is coded with an int. 0 is the bottom left player.
 *1 is the upper right player.
@@ -52,12 +62,7 @@ int player_adjacent(char player_symbol,int i,int j, char board[SIZE][SIZE]) {
 void world_update(int player, char color,char board[SIZE][SIZE])
 {
     int modified = 0;
-    char player_symbol;
-    if (player == 0) {
-      player_symbol = 'v';
-    } else {
-      player_symbol ='^';
-    }
+    char player_symbol = symbol(player);
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
           if (board[i][j] == color && player_adjacent(player_symbol,i,j, board)) {
@@ -71,14 +76,32 @@ void world_update(int player, char color,char board[SIZE][SIZE])
     }
 }
 
+void play_turn(int* player_pointer, char board[SIZE][SIZE]) {
+  char c;
+  char color;
+  char player_symbol = symbol(*player_pointer);
+  printf("\nWhich color ? (%i turn)\n",*player_pointer);
+  scanf("%c",&color);
+  while((c = getchar()) != '\n' && c != EOF) {}
+  for (int i = 0; i< 50;i++) {
+    printf("\n");
+  }
+  world_update(*player_pointer,color,board);
+  print_board(board, SIZE);
+  *player_pointer = (*player_pointer + 1) % 2;
+  printf("%i",*player_pointer);
+  play_turn(player_pointer,board);
+}
+
+
 /** Program entry point */
 int main(void) {
 	char board[SIZE][SIZE];
-
 	srand(time(NULL));
-
 	init_board(board, SIZE);
-    print_board(board, SIZE);
-
+  print_board(board, SIZE);
+  int player = 0;
+  int* player_pointer = &player;
+  play_turn(player_pointer,board);
     return 0;
 }
