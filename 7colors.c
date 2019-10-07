3fb56fb128e5dc038b9006cb120cd9786c6c3d1b
skepar <skepar@skepar.org>
2019-10-07 15:30:37 +0200

    Refactoring, using structs and mallocs

--- 7colors.c
+++ 7colors.c
@@ -3,139 +3,175 @@
 #include <time.h>
 #define SIZE 30
 
-void print_board(char board [SIZE][SIZE], int size) {
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            printf("%c", board[i][j]);
-        }
+typedef struct Game Game;
+typedef enum Status Status;
+
+enum Status { A_PLAYING, B_PLAYING, A_WON, B_WON, DRAW };
+
+struct Game {
+	char** board;
+	Status current;
+    int a_score;
+    int b_score;
+	char game_mode;     // set to 1 if the second player is a computer, 0 otherwise
+};  
+
+void print_board(char** board) {
+    for (int i = 0; i < 50; i++) {
         printf("\n");
     }
+    
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; j++) {
+			printf("%c", board[i][j]);
+		}
+		printf("\n");
+	}
 }
 
-void init_board(char board[SIZE][SIZE], int size) {
+char** init_board() {
+    char** board = malloc(SIZE * sizeof(char*));
+    
 	for (int i = 0; i < SIZE; i++) {
+		board[i] = malloc(SIZE * sizeof(char));
         for (int j = 0; j < SIZE; j++) {
-			      board[i][j]=rand()%7+65;
-        }
-    }
-    board[SIZE-1][0]='v';
-    board[0][SIZE-1]='^';
+			board[i][j]=rand()%7+65;
+		}
+	}
+	board[SIZE-1][0]='v';
+	board[0][SIZE-1]='^';
+    
+    return board;
+}
+
+void free_board(char** board) {
+    for (int i = 0; i < SIZE; i++) {
+		free(board[i]);
+	}
+    free(board);
 }
 
 /** Returns 1 if the cell (i,j) is adjacent to a cell possessed by player*/
-int player_adjacent(char player_symbol,int i,int j, char board[SIZE][SIZE]) {
-    if (i-1 >= 0) {
-      if (board[i-1][j]==player_symbol) {
-        return 1;
-      }
+char player_adjacent(Game* game_ptr,int i,int j) {
+    char player_symbol;
+    
+    if (game_ptr->current == A_PLAYING) {
+        player_symbol = 'v';
+    } else {
+        player_symbol = '^';
     }
-    if (i+1 < SIZE) {
-      if (board[i+1][j]==player_symbol) {
-        return 1;
-      }
-    }
-    if (j-1 >= 0) {
-      if (board[i][j-1]==player_symbol) {
-        return 1;
-      }
-    }
-    if (j+1 < SIZE) {
-      if (board[i][j+1]==player_symbol) {
-        return 1;
-      }
-    }
-    return 0;
-  }
 
-char symbol(int player) {
-  if (player == 0) {
-    return 'v';
-  } else {
-    return '^';
-  }
+	if (i-1 >= 0) {
+        if (game_ptr->board[i-1][j]==player_symbol) {
+            return 1;
+		}
+	}
+	if (i+1 < SIZE) {
+		if (game_ptr->board[i+1][j]==player_symbol) {
+            return 1;
+		}
+	}
+	if (j-1 >= 0) {
+		if (game_ptr->board[i][j-1]==player_symbol) {
+            return 1;
+		}
+	}
+	if (j+1 < SIZE) {
+		if (game_ptr->board[i][j+1]==player_symbol) {
+            return 1;
+		}
+	}
+	return 0;
 }
 
 /**Updates the board when a player has played and chosen a color
 *Note : The player is coded with an int. 0 is the bottom left player.
 *1 is the upper right player.
 */
-void world_update(int player, char color,char board[SIZE][SIZE])
-{
-    int modified = 0;
-    char player_symbol = symbol(player);
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-          if (board[i][j] == color && player_adjacent(player_symbol,i,j, board)) {
-            board[i][j]=player_symbol;
-            modified = 1;
-          }
-        }
+void world_update(Game* game_ptr, char color) {
+	char player_symbol;
+    char modified = 0;
+    
+    if (game_ptr->current == A_PLAYING) {
+        player_symbol = 'v';
+    } else {
+        player_symbol = '^';
     }
-    if (modified == 1) {
-      world_update(player, color, board);
-    }
+    
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; j++) {
+			if (game_ptr->board[i][j] == color && player_adjacent(game_ptr, i, j)) {
+                game_ptr->board[i][j] = player_symbol;
+                if (game_ptr->current == A_PLAYING) {
+                    game_ptr->a_score++;
+                } else {
+                    game_ptr->b_score++;                
+                }
+                modified = 1;
+			}
+		}
+	}
+    
+	if (modified == 1) {
+		world_update(game_ptr, color);
+	}
 }
 
-double rate_symbol(char symbol, char board[SIZE][SIZE]) {
-  int number_symbol = 0;
-  double rate;
-  for (int i = 0; i < SIZE; i++) {
-      for (int j = 0; j < SIZE; j++) {
-        if (board[i][j] == symbol) {
-          number_symbol++;
-        }
-      }
+void play_turn(Game* game_ptr) {
+    char c;
+	char color;
+    char player_symbol;
+    
+    if (game_ptr->current == A_PLAYING) {
+        player_symbol = 'v';
+    } else {
+        player_symbol = '^';
     }
-  rate = ( (double) number_symbol/ (double) (SIZE * SIZE) ) * 100;
-  return rate;
+    
+    double rate_Aplayer = ((double) game_ptr->a_score/(double) (SIZE * SIZE)) * 100;
+	double rate_Bplayer = ((double) game_ptr->b_score/(double) (SIZE * SIZE)) * 100;
+	printf("\nPlayer v owns %f %% of the map", rate_Aplayer);
+	printf("\nPlayer ^ owns %f %% of the map", rate_Bplayer);
+    
+	printf("\nWhich color ? (%c turn)\n",player_symbol);
+	scanf("%c",&color);
+	while((c = getchar()) != '\n' && c != EOF) {}
+    
+	world_update(game_ptr, color);
+	game_ptr->current = (game_ptr->current + 1) % 2;    //swap A_PLAYING and B_PLAYING
+
+	if (rate_Aplayer > 50) {
+        game_ptr->current = A_WON;
+	}
+	if (rate_Bplayer > 50) {
+        game_ptr->current = B_WON;
+	}
+	if (rate_Aplayer == rate_Aplayer && rate_Aplayer >= 50) {
+        game_ptr->current = DRAW;
+	}
 }
-
-int play_turn(int* player_pointer, char board[SIZE][SIZE]) {
-  char c;
-  char color;
-  char player_symbol = symbol(*player_pointer);
-  double rate_player_0;
-  double rate_player_1;
-  printf("\nWhich color ? (%c turn)\n",player_symbol);
-  scanf("%c",&color);
-  while((c = getchar()) != '\n' && c != EOF) {}
-  for (int i = 0; i< 50;i++) {
-    printf("\n");
-  }
-  world_update(*player_pointer,color,board);
-  print_board(board, SIZE);
-  *player_pointer = (*player_pointer + 1) % 2;
-  rate_player_0 =rate_symbol('v',board);
-  rate_player_1 =rate_symbol('^',board);
-  printf("\nPlayer v owns %f %% of the map", rate_player_0);
-  printf("\nPlayer ^ owns %f %% of the map", rate_player_1);
-  if (rate_player_0 > 50) {
-    return 0;
-  }
-  if (rate_player_1 > 50) {
-    return 1;
-  }
-  if (rate_player_0 == rate_player_1 && rate_player_0 >= 50) {
-    return 2;
-  }
-  play_turn(player_pointer,board);
-
-}
-
 
 /** Program entry point */
 int main(void) {
-	char board[SIZE][SIZE];
-	srand(time(NULL));
-	init_board(board, SIZE);
-  print_board(board, SIZE);
-  int player = 0;
-  int* player_pointer = &player;
-  int winner = play_turn(player_pointer,board);
-  if (winner == 2) {
-    printf("\n\nDraw\n");
-  } else {
-    printf("\n\nPlayer %c wins\n",symbol(winner));
-  }
-    return 0;
+    srand(time(NULL));
+    Game game = { init_board(), B_PLAYING, 0, 0, 0 };
+    
+    while (game.current == A_PLAYING || game.current == B_PLAYING) {
+        print_board(game.board);
+        play_turn(&game);
+    }
+    
+    print_board(game.board);
+    
+	if (game.current == DRAW) {
+        printf("\n\nDraw\n");
+	} else if (game.current == A_WON) {
+        printf("\n\nPlayer v wins\n");
+	} else {
+        printf("\n\nPlayer ^ wins\n");
+    }
+    
+    free_board(game.board);
+    
+	return 0;
 }
